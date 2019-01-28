be390cbed7b1e6d39be5841e073a9a5f74d69f64
zhoudeguang <zhoudeguang@future.com.cn>
2019-01-28 15:51:46 +0800

    96: numTrees
    96. 不同的二叉搜索树

--- leetcode/Solution.cpp
+++ leetcode/Solution.cpp
@@ -627,6 +627,20 @@ vector<TreeNode*> Solution::generateTrees(int n)
     return *createBST(1, n);
 }
 
+//96
+int Solution::numTrees(int n)
+{
+    vector<int> dp(n + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            dp[i] += (dp[j] * dp[i - 1 - j]);
+        }
+    }
+
+    return dp[n];
+}
+
 //98
 void Solution::inOderIsValidBST(TreeNode* root)
 {
